3f16b0f6df1ceb0c2b0d1e50d5a4ada561275efd
Fatema-binbytes <fatema.binbytes@gmail.com>
2019-08-26 14:29:26 +0530

    dispatched style

--- example/src/component/backgoundView.js
+++ example/src/component/backgoundView.js
@@ -1,18 +1,11 @@
 import React, { Component } from "react";
-import { View, TouchableOpacity, Text } from "react-native";
+import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
 import Icon from "react-native-vector-icons/MaterialIcons";
 
 class BackComponent extends Component {
   render() {
     return (
-      <View
-        style={{
-          width: "100%",
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between"
-        }}
-      >
+      <View style={styles.container}>
         <TouchableOpacity>
           <Text style={{ color: "white", fontSize: 18 }}>{"Undo"}</Text>
         </TouchableOpacity>
@@ -23,4 +16,12 @@ class BackComponent extends Component {
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  }
+});
 export default BackComponent;
--- src/backgoundView.js
+++ src/backgoundView.js
@@ -1,16 +1,11 @@
 import React, { Component} from 'react'
-import { View, TouchableOpacity, Text } from 'react-native'
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native'
 import Icon from "react-native-vector-icons/MaterialIcons";
 
 class BackgoundView extends Component {
    render() {
         return (
-            <View style={{
-                    width: "100%",
-                    flexDirection: "row",
-                    alignItems: "center",
-                    justifyContent: "space-between"
-                  }}>
+            <View style={styles.container}>
                 <TouchableOpacity>
                     <Text style={{ color: "white", fontSize: 18 }}>{'Undo'}</Text>
                 </TouchableOpacity>
@@ -21,4 +16,12 @@ class BackgoundView extends Component {
         )
     }
 }
+const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between"
+      }
+})
 export default BackgoundView; 
