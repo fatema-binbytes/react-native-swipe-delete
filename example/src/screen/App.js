db9b819ffe33bbf43490ffc96eab4d7413cbca19
Fatema-binbytes <fatema.binbytes@gmail.com>
2019-08-28 17:56:11 +0530

    remove " ; in example file

--- example/src/screen/App.js
+++ example/src/screen/App.js
@@ -1,112 +1,109 @@
-import React, { Component } from "react";
-import Item from "react-native-swipe-delete-view";
+import React, { Component } from 'react'
+import Item from 'react-native-swipe-delete-view'
 
-import BackComponent from '../component/backgroundRow';
+import BackgoundView from '../component/backgoundView'
 
 
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-       data: [
+      data: [
         {
-          id:1,
-          displayPicture: require("../../images/BinStorm.png"),
-          chat: "Running application ListDeleteImage with appParams:",
-          time: "3:14 pm",
-          userIdName: "BT-IDBWRF"
-        },
-        {
-          id:2,
-          displayPicture: require("../../images/BinStorm.png"),
-          chat: "Running application ListDeleteImage with appParams:",
-          time: "3:14 pm",
-          userIdName: "IG-Idea"
-        },
-        { id:3,
-          displayPicture: require("../../images/BinStorm.png"),
-          chat: "Running application ListDeleteImage with appParams:",
-          time: "3:14 pm",
-          userIdName: "59823"
-        },
-        { 
-          id:4,
-          displayPicture: require("../../images/BinStorm.png"),
-          chat: "Running application ListDeleteImage with appParams:",
-          time: "3:14 pm",
-          userIdName: "BT-IDBWRF"
+          displayPicture: require('../../images/BinStorm.png'),
+          chat: 'Running application ListDeleteImage with appParams:',
+          time: '3:14 pm',
+          userIdName: 'BT-IDBWRF'
         },
         {
-          displayPicture: require("../../images/BinStorm.png"),
-          chat: "Running application ListDeleteImage with appParams:",
-          time: "3:14 pm",
-          userIdName: "BT-IDBWRF"
+          displayPicture: require('../../images/BinStorm.png'),
+          chat: 'Running application ListDeleteImage with appParams:',
+          time: '3:14 pm',
+          userIdName: 'IG-Idea'
         },
         {
-          displayPicture: require("../../images/BinStorm.png"),
-          chat: "Running application ListDeleteImage with appParams:",
-          time: "3:14 pm",
-          userIdName: "IG-Idea"
+          displayPicture: require('../../images/BinStorm.png'),
+          chat: 'Running application ListDeleteImage with appParams:',
+          time: '3:14 pm',
+          userIdName: '59823'
         },
         {
-          displayPicture: require("../../images/BinStorm.png"),
-          chat: "Running application ListDeleteImage with appParams:",
-          time: "3:14 pm",
-          userIdName: "59823"
+          displayPicture: require('../../images/BinStorm.png'),
+          chat: 'Running application ListDeleteImage with appParams:',
+          time: '3:14 pm',
+          userIdName: 'BT-IDBWRF'
         },
         {
-          displayPicture: require("../../images/BinStorm.png"),
-          chat: "Running application ListDeleteImage with appParams:",
-          time: "3:14 pm",
-          userIdName: "BT-IDBWRF"
+          displayPicture: require('../../images/BinStorm.png'),
+          chat: 'Running application ListDeleteImage with appParams:',
+          time: '3:14 pm',
+          userIdName: 'BT-IDBWRF'
         },
         {
-          displayPicture: require("../../images/BinStorm.png"),
-          chat: "Running application ListDeleteImage with appParams:",
-          time: "3:14 pm",
-          userIdName: "BT-IDBWRF"
+          displayPicture: require('../../images/BinStorm.png'),
+          chat: 'Running application ListDeleteImage with appParams:',
+          time: '3:14 pm',
+          userIdName: 'IG-Idea'
         },
         {
-          displayPicture: require("../../images/BinStorm.png"),
-          chat: "Running application ListDeleteImage with appParams:",
-          time: "3:14 pm",
-          userIdName: "IG-Idea"
+          displayPicture: require('../../images/BinStorm.png'),
+          chat: 'Running application ListDeleteImage with appParams:',
+          time: '3:14 pm',
+          userIdName: '59823'
         },
         {
-          displayPicture: require("../../images/BinStorm.png"),
-          chat: "Running application ListDeleteImage with appParams:",
-          time: "3:14 pm",
-          userIdName: "59823"
+          displayPicture: require('../../images/BinStorm.png'),
+          chat: 'Running application ListDeleteImage with appParams:',
+          time: '3:14 pm',
+          userIdName: 'BT-IDBWRF'
         },
         {
-          displayPicture: require("../../images/BinStorm.png"),
-          chat: "Running application ListDeleteImage with appParams:",
-          time: "3:14 pm",
-          userIdName: "BT-IDBWRF"
+          displayPicture: require('../../images/BinStorm.png'),
+          chat: 'Running application ListDeleteImage with appParams:',
+          time: '3:14 pm',
+          userIdName: 'BT-IDBWRF'
+        },
+        {
+          displayPicture: require('../../images/BinStorm.png'),
+          chat: 'Running application ListDeleteImage with appParams:',
+          time: '3:14 pm',
+          userIdName: 'IG-Idea'
+        },
+        {
+          displayPicture: require('../../images/BinStorm.png'),
+          chat: 'Running application ListDeleteImage with appParams:',
+          time: '3:14 pm',
+          userIdName: '59823'
+        },
+        {
+          displayPicture: require('../../images/BinStorm.png'),
+          chat: 'Running application ListDeleteImage with appParams:',
+          time: '3:14 pm',
+          userIdName: 'BT-IDBWRF'
         }
       ],
-    };
+    }
   }
   
   onSwipeLeft(i) {
-   //this.state.data.splice(i, 1);
+   //this.state.data.splice(i, 1)
     console.log(i)
   }
 
   onSwipeRight(i) {
-    //this.state.data.splice(i, 1);
-     console.log(i)
-   }
+    //this.state.data.splice(i, 1)
+    console.log(i)
+  }
    render() {
     return ( 
       <Item 
         onSwipeLeft={i => this.onSwipeLeft(i)}
         onSwipeRight={i => this.onSwipeRight(i)} 
         data={this.state.data} 
-        backComponent={<BackComponent/>}
+        backgoundView={<BackgoundView />}
       />
     )
   }
 }
 
-export default App;
+export default App
